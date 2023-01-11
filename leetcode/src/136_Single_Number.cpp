c34f52c8847bf2c2651aa90e5a9bef436aeb53a9
Hyun Sik Yoon (Eric Yoon) <63768207+hyunsik-yoon@users.noreply.github.com>
2023-01-10 19:03:21 -0800

    Add 136 (#44)
    Code for 136 was added.
    
    Signed-off-by: Hyun Sik Yoon (Eric Yoon) <hyunsik.yoon.1024@gmail.com>

---
+++ leetcode/src/136_Single_Number.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+// 136_Single_Number
+
+// https://leetcode.com/problems/single-number/description/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        // runtime complexity should be O(n)
+        // space complexity should be O(1)
+        //
+        // brute force O(n^2)
+        // set space O(n)
+        // sort O(nlogn), space O(nlogn)
+
+        // REMEMBER
+        //
+        // Method 1.
+        //  for
+        //   k1, k2, k3, ... ki, kn
+        //   k1, k2, k3, ...     kn
+        //  calculate
+        //   2*(all number) - 2(k1, k2, k3, ...   kn) + ki = ki
+        //
+        // Method 2.
+        // XOR
+        // -> a⊕0=a
+        // -> a⊕a=0
+        // -> a⊕b⊕a=(a⊕a)⊕b=0⊕b=b
+
+        int ans = 0;
+        for (const auto &num : nums)
+        {
+            ans ^= num;
+        }
+
+        return ans;
+    }
+};
