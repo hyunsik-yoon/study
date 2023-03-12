81dd87463031d57f71b0b97d53d32c482f2bb359
Hyun Sik Yoon (Eric Yoon) <63768207+hyunsik-yoon@users.noreply.github.com>
2023-03-11 20:35:42 -0800

    Modify 973 with priority_queue (#123)
    Code for 973 was modified to use priority_queue.
    
    Signed-off-by: Hyun Sik Yoon (Eric Yoon) <hyunsik.yoon.1024@gmail.com>

--- leetcode/src/973_K_Closest_Points_to_Origin.cpp
+++ leetcode/src/973_K_Closest_Points_to_Origin.cpp
@@ -1,17 +1,3 @@
-#include <algorithm>
-#include <cassert>
-#include <cmath>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <stack>
-#include <unordered_set>
-#include <vector>
-
-using namespace std;
-
 // 973_K_Closest_Points_to_Origin
 
 // https://leetcode.com/problems/k-closest-points-to-origin/description/
@@ -20,7 +6,7 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
     {
-        return method_nlogn(points, k);
+        // return method_nlogn(points, k);
         return method_nlogk(points, k);
     }
 
@@ -38,13 +24,17 @@ private:
 
     vector<vector<int>> method_nlogk(vector<vector<int>>& points, int k)
     {
-        // maintain maxheap of size k
-        auto compare = [](vector<int> &a, vector<int> &b) {
+        // maintain minheap of size k
+        auto compare = [](const vector<int> &a, const vector<int> &b) {
             return a[0]*a[0] + a[1]*a[1] < b[0]*b[0] + b[1]*b[1];
         };
 
         // REMEMBER
-        //      heap
+        //      make_heap을 쓰고 pop_heap()의 전/후의 ans.size()는 동일하다.
+        //      pop_heap()는 head 를 tail 에 위치시켜버림...
+        //      그래서 heap의 사이즈를 알기가 좀 애매하다.
+        //      따라서, make_heap 말고 priority _queue를 쓰는게 나을 듯.
+        /*
         vector<vector<int>> ans;
         make_heap(ans.begin(), ans.end(), compare);
 
@@ -54,12 +44,35 @@ private:
             // place new item at the end
             ans.emplace_back(point);
             // heapify
-            push_heap(ans.begin(), ans.end());
+            push_heap(ans.begin(), ans.end(), compare);
 
             if (ans.size() > k)
-                pop_heap(ans.begin(), ans.end());   // ans.front()를 삭제후 heapify
+                pop_heap(ans.begin(), ans.end(), compare);   // ans.front()를 삭제후 heapify
             // Note: 제일 큰 값을 가져오려면
             // ans.front() 를 보면 됨.
+        }
+
+        return ans;
+        */
+
+        // REMEMBER
+        //   priority_queue declaration
+        priority_queue<vector<int>, vector<vector<int>>, decltype(compare)> pq(compare);
+
+        for (auto &point: points)
+        {
+            pq.push(point);
+
+            if (pq.size() > k)
+                pq.pop();
+        }
+
+        vector<vector<int>> ans;
+        while(!pq.empty())
+        {
+            auto point = pq.top();
+            pq.pop();
+            ans.emplace_back(point);
         }
 
         return ans;
