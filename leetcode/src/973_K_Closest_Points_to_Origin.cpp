4cebffdfd6c2651a5b4d6f4f6506715d3c157744
Hyun Sik Yoon (Eric Yoon) <63768207+hyunsik-yoon@users.noreply.github.com>
2023-03-11 19:25:38 -0800

    Add nlogk solution into 973 (#122)
    nlogk solution code for 973 was added.
    
    Signed-off-by: Hyun Sik Yoon (Eric Yoon) <hyunsik.yoon.1024@gmail.com>

--- leetcode/src/973_K_Closest_Points_to_Origin.cpp
+++ leetcode/src/973_K_Closest_Points_to_Origin.cpp
@@ -20,6 +20,13 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
     {
+        return method_nlogn(points, k);
+        return method_nlogk(points, k);
+    }
+
+private:
+    vector<vector<int>> method_nlogn(vector<vector<int>>& points, int k)
+    {
         auto compare = [](vector<int> &a, vector<int> &b) {
             return a[0]*a[0] + a[1]*a[1] < b[0]*b[0] + b[1]*b[1];
         };
@@ -27,5 +34,34 @@ public:
         std::sort(points.begin(), points.end(), compare);
 
         return vector<vector<int>>(points.begin(), points.begin() + k);
+    }
+
+    vector<vector<int>> method_nlogk(vector<vector<int>>& points, int k)
+    {
+        // maintain maxheap of size k
+        auto compare = [](vector<int> &a, vector<int> &b) {
+            return a[0]*a[0] + a[1]*a[1] < b[0]*b[0] + b[1]*b[1];
+        };
+
+        // REMEMBER
+        //      heap
+        vector<vector<int>> ans;
+        make_heap(ans.begin(), ans.end(), compare);
+
+        // maintain ans of size k
+        for (auto &point : points)
+        {
+            // place new item at the end
+            ans.emplace_back(point);
+            // heapify
+            push_heap(ans.begin(), ans.end());
+
+            if (ans.size() > k)
+                pop_heap(ans.begin(), ans.end());   // ans.front()를 삭제후 heapify
+            // Note: 제일 큰 값을 가져오려면
+            // ans.front() 를 보면 됨.
+        }
+
+        return ans;
     }
 };
