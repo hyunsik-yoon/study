7d1efa3915950dae0fd4f53b3a675ff01eaf83f5
Hyun Sik Yoon (Eric Yoon) <hyunsik.yoon.1024@gmail.com>
2023-03-17 00:00:16 -0700

    Add comment for 1010
    Comment for 1010 was added.
    
    Signed-off-by: Hyun Sik Yoon (Eric Yoon) <hyunsik.yoon.1024@gmail.com>

--- leetcode/src/1010_Pairs_of_Songs_With_Total_Durations_Divisible_by_60.cpp
+++ leetcode/src/1010_Pairs_of_Songs_With_Total_Durations_Divisible_by_60.cpp
@@ -35,6 +35,11 @@ public:
             int reminder = t % 60;                 // [0, 59]
             int required = (60 - reminder) % 60;   // [0, 59]
 
+            // REMEMBER
+            //  변수의 scope을 유의하라.
+            // 첨에는 required 같은 경우 % 60 을 안해주었다. 그런데 scope 이 [0, 59]여야 하는데,
+            // reminder 가 0 인경우 60 - 0 = 60 이 되어 의도와는 다르게 scope을 벗어났다.
+
             auto found = reminder_count.find(required);
             if (found != reminder_count.end())
             {
