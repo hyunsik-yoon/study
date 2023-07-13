98fbc90432dfaaddc415d78733d802cdfe2d2857
Hyun Sik Yoon (Eric Yoon) <63768207+hyunsik-yoon@users.noreply.github.com>
2023-07-12 17:49:51 -0700

    Revert "Add 392 with code (#200)"
    This reverts commit 8870e4597d1d2f6765b46eac8e2d96d31773bbad.

--- leetcode/src/392_Is_Subsequence.cpp
+++ leetcode/src/392_Is_Subsequence.cpp
@@ -12,31 +12,3 @@
 
 using namespace std;
 
-// 392. Is Subsequence
-//https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=top-interview-150
-
-class Solution {
-public:
-    bool isSubsequence(string s, string t) {
-        int sk = 0;
-        int tk = 0;
-
-        while (sk < s.length() && tk < t.length())
-        {
-            if (s[sk] == t[tk])
-            {
-                ++sk;
-                ++tk;
-            }
-            else
-            {
-                tk++;
-            }
-        }
-
-        if (sk == s.length())
-            return true;
-        else
-            return false;
-    }
-};
